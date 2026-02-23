c1aa6ad2741091700fff420ef8de7e6bc6721663
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2026-02-23 15:05:40 +0100

    Add listener to connect foirequests

--- froide_pressconference/apps.py
+++ froide_pressconference/apps.py
@@ -6,3 +6,10 @@ class FroidePressconferenceConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "froide_pressconference"
     verbose_name = _("Press Conferences")
+
+    def ready(self):
+        from froide.foirequest.models import FoiRequest
+
+        from .listeners import create_link
+
+        FoiRequest.request_sent.connect(create_link)
---
+++ froide_pressconference/listeners.py
@@ -0,0 +1,40 @@
+from .models import PressConference, Section
+
+
+def create_link(sender, **kwargs):
+    reference = kwargs.get("reference")
+    if not reference:
+        reference = sender.reference
+    if not reference:
+        return
+    if not reference.startswith("pressconference:"):
+        return
+    namespace, pc_section_value = reference.split(":", 1)
+    try:
+        section_id, pc_id = pc_section_value.split("@", 1)
+    except (ValueError, IndexError):
+        return
+
+    if not section_id or not pc_id:
+        return
+
+    try:
+        pc_id = int(pc_id)
+    except ValueError:
+        return
+    try:
+        section_id = int(section_id)
+    except ValueError:
+        return
+
+    try:
+        pc = PressConference.objects.get(pk=pc_id)
+    except PressConference.DoesNotExist:
+        return
+
+    try:
+        section = Section.objects.get(press_conference=pc, pk=section_id)
+    except Section.DoesNotExist:
+        return
+
+    section.foirequests.add(sender)
